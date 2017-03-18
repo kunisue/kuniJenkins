132073516539e93a214e7bff868f7c24a4f94cd7
kunisue <kunitatu777@gmail.com>
2017-03-18 20:52:55 +0900

    2nd commit.

--- src/main/java/servlet/IndexServ.java
+++ src/main/java/servlet/IndexServ.java
@@ -8,11 +8,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import util.KuniUtils;
+
 public class IndexServ extends HttpServlet {
 
 	private static final long serialversionUID = -1L;
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		System.out.println(KuniUtils.getMsg("abc"));
 
 		String disp = "/index.jsp";
 		RequestDispatcher dispatch = req.getRequestDispatcher(disp);
---
+++ src/main/java/util/KuniUtils.java
@@ -0,0 +1,7 @@
+package util;
+
+public class KuniUtils {
+    public static String getMsg(String str) {
+		return str;
+	}
+}
